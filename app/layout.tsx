da66b5362590e9d10cb853385503d296e5baddbb
Hasnain Khan🏴 <official.thevm@gmail.com>
2025-02-23 02:19:04 +0500

    done

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,20 +2,20 @@ import { Navbar } from "@/components";
 import "@/styles/globals.css";
 import type { Metadata } from "next";
 export const metadata: Metadata = {
-	title: "TheFlow Party",
-	description: "Flow Party Website Clone By Devwithzain",
+  title: "The Flow Party",
+  description: "Flow Party Website Clone By Devwithzain",
 };
 export default function RootLayout({
-	children,
+  children,
 }: {
-	children: React.ReactNode;
+  children: React.ReactNode;
 }) {
-	return (
-		<html lang="en">
-			<body>
-				<Navbar />
-				{children}
-			</body>
-		</html>
-	);
+  return (
+    <html lang="en">
+      <body>
+        <Navbar />
+        {children}
+      </body>
+    </html>
+  );
 }
